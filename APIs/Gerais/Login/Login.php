ffbcbf0c6965ba1d4eac09bffa76b27b026f6282
Carlos <carlos.soares@shcompanytechnology.com>
2024-07-06 15:40:00 -0300

    apis de login

---
+++ APIs/Gerais/Login/Login.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once('conexaoSH.php');
+
+$USUARIO= $_GET['usuario'];
+$SENHA= $_GET['senha'];
+
+$query = $pdo->query("SELECT * from Usuarios where Usuario= '$USUARIO' and Senha= '$SENHA'");
+
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+for ($i = 0; $i < count($res); $i++) {
+    foreach ($res[$i] as $key => $value) {
+    }
+    $dados[] = array(
+                  
+        'id' => $res[$i]['id'],     
+    );
+}
+
+if (count($res) > 0) {
+    $result = json_encode(array('success' => true, 'result' => $dados));
+} else {
+    $result = json_encode(array('success' => false, 'result' => '0'));
+}
+echo $result;
